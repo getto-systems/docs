07faa73ce00fd0abee12fac53e04bc5d13046a18
shun <shun.fix9@gmail.com>
2019-09-15 02:44:55 +0000

    add: deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+build_main(){
+  local version
+  local domain
+  local metadata
+
+  version=$(cat .release-version)
+
+  sed -i -e 's|baseURL = "https://\([^/]\+\)/dev/"|baseURL = "https://\1/'"$version"'/"|' config.toml
+  domain=$(grep "baseURL" config.toml | sed -e 's|.*baseURL = "https://\([^/]\+\)/.*|\1|')
+
+  hugo
+
+  metadata=$(node metadata.js)
+
+  aws s3 cp \
+    --acl private \
+    --cache-control "public, max-age=31536000" \
+    --metadata "$metadata" \
+    --recursive \
+    public s3://$domain/$version
+}
+
+build_main
