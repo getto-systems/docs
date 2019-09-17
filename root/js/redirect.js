3cfad440722033096911e1f52d0f305f06fba595
shun <shun.fix9@gmail.com>
2019-09-17 19:28:15 +0000

    fix: redirect

--- root/js/redirect.js
+++ root/js/redirect.js
@@ -1,13 +1,5 @@
-var pathnames = location.pathname.split("/");
-var version;
-var path;
-if (!pathnames[1] || !(pathnames[1] + "").match(/^[0-9.-]+/)) {
-  version = "1.0.0";
-  path = "";
-} else {
-  version = pathnames[1];
-  path = pathnames.slice(2).join("/") + location.search;
-}
-GettoDetect().from_current_version(version,function(href) {
-  location.href = href.replace(/index.html$/, "") + path;
-});
+(function(info) {
+  FindNextVersion.find({ from: info.version, url: FindNextVersion.url.index }).then(function(version) {
+    location.href = "/" + version + "/" + info.path + location.search;
+  });
+})(FindNextVersion.parse_pathname(location.pathname));
