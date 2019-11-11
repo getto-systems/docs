3661dcd75129102e3765ea884409072a4d9af540
shun <shun.fix9@gmail.com>
2019-11-11 12:56:44 +0000

    fix: fallback_version

--- root/js/redirect.js
+++ root/js/redirect.js
@@ -2,4 +2,4 @@
   FindNextVersion.find({ from: info.version, url: FindNextVersion.url.index }).then(function(version) {
     location.href = "/" + version + "/" + info.path + location.search;
   });
-})(FindNextVersion.parse_pathname(location.pathname));
+})(FindNextVersion.parse_pathname(location.pathname, { fallback_version: "1.23.0" }));
