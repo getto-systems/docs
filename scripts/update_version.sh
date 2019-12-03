fc817eafb6420533fc07cb6e0e900cc4b21c54ad
shun <shun.fix9@gmail.com>
2019-12-03 16:31:57 +0000

    fix: gitlab-ci

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -4,10 +4,8 @@
 ./bin/update_hugo.sh
 
 if [ "$(git status -s Dockerfile)" ]; then
-  getto-hangar-build.sh
-
   git add Dockerfile
   git commit -m "update: tool version"
 
-  curl https://raw.githubusercontent.com/getto-systems/version-dump/master/bin/request.sh | bash -s -- ./.update-version-message.sh
+  curl https://trellis.getto.systems/ci/bump-version/1.2.2/request.sh | bash -s -- ./.update-version-message.sh
 fi
